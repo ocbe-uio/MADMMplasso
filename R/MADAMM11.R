d1abd6c0c8f290a2315ba50843f62e373d2610eb
Theo-qua <asenso7@ymail.com>
2022-09-08 14:01:43 +0200

    Update to include examples

--- R/MADAMM11.R
+++ R/MADAMM11.R
@@ -456,6 +456,65 @@ admm.MADMMplasso<-function(beta0,theta0,beta,beta_hat,theta,rho1,X,Z,max_it,W_ha
 #' @param e.abs absolute error for the admm
 #' @param e.rel relative error for the admm
 #' @param gg penalty term for the tree structure
+#' 
+#' 
+#' 
+#' 
+#' @examples
+#' Train the model
+#' generate some data
+#' set.seed(1235)
+#' N = 100 ; p =500;nz=4; K=nz
+#' X <- matrix(rnorm(n = N * p), nrow = N, ncol = p)
+#' mx=colMeans(X)
+#' sx=sqrt(apply(X,2,var))
+#' X=scale(X,mx,sx)
+#' X=matrix(as.numeric(X),N,p)
+#' Z =matrix(rnorm(N*nz),N,nz)
+#' mz=colMeans(Z)
+#' sz=sqrt(apply(Z,2,var))
+#' Z=scale(Z,mz,sz)
+#' beta_1 <- rep(x = 0, times = p); beta_2<-rep(x = 0, times = p);beta_3<-rep(x = 0, times = p);beta_4<-rep(x = 0, times = p);beta_5<-rep(x = 0, times = p);beta_6<-rep(x = 0, times = p)
+#' 
+#' 
+#' beta_1[1:5] <- c(2, 2, 2, 2,2); beta_2[1:5]<-c(2, 2, 2, 2,2); beta_3[6:10]<-c(2, 2, 2, -2,-2); beta_4[6:10] <- c(2, 2, 2, -2,-2); beta_5[11:15] <- c(-2,  -2,-2, -2,-2);beta_6[11:15] <- c(-2, -2, -2, -2,-2)
+#' 
+#' Beta<-cbind(beta_1,beta_2,beta_3,beta_4,beta_5,beta_6)
+#' colnames(Beta)<-c(1:6)
+#' 
+#' 
+#' theta<-array(0,c(p,K,6))
+#' theta[1,1,1]<-2;theta[3,2,1]<-2;theta[4,3,1]<- -2;theta[5,4,1]<- -2
+#' theta[1,1,2]<-2;theta[3,2,2]<-2;theta[4,3,2]<- -2;theta[5,4,2]<- -2
+#' theta[6,1,3]<-2;theta[8,2,3]<-2;theta[9,3,3]<- -2;theta[10,4,3]<- -2
+#' theta[6,1,4]<-2;theta[8,2,4]<-2;theta[9,3,4]<- -2;theta[10,4,4]<- -2
+#' theta[11,1,5]<-2;theta[13,2,5]<-2;theta[14,3,5]<- -2;theta[15,4,5]<- -2
+#' theta[11,1,6]<-2;theta[13,2,6]<-2;theta[14,3,6]<- -2;theta[15,4,6]<- -2
+#' 
+#' library(MASS)
+#' pliable = matrix(0,N,6)
+#' for (e in 1:6) {
+#' pliable[,e]<-	compute_pliable(X, Z, theta[,,e])
+#' }
+#' 
+#' esd<-diag(6)
+#' e<-mvrnorm(N,mu=rep(0,6),Sigma=esd)
+#' y_train<-X%*%Beta+pliable+e
+#' y=y_train
+#' #colnames(y)<-c(1:6)
+#' colnames(y)<- c( paste("y",1:(ncol(y)),sep = "") )
+#' TT=tree.parms(y)
+#' plot(TT$h_clust)
+#' gg1<-c(.5,0.0001)
+#' cl=detectCores()
+#' nlambda = 50;e.abs=1E-3;e.rel=1E-3;alpha=.5
+#'  fit<-MADMMplasso(X,Z,y,alpha=alpha,my_lambda=NULL,lambda_min=0.01,max_it=5000,e.abs=e.abs,e.rel=e.rel,maxgrid=50,nlambda = nlambda, rho=5,tree = TT,my_print = F,alph=1,parallel =F,pal=1,gg=gg1,tol=1E-3,cl=cl-2 )
+#'  plot(fit) 
+
+
+
+
+
 
 
 
@@ -1211,6 +1270,67 @@ errfun.gaussian<-function(y,yhat,w=rep(1,length(y))){  ( w*(y-yhat)^2) }
 #' @param nfolds  number of cross-validation folds
 #' #' @param foldid  vector with values in 1:K, indicating folds for K-fold CV. Default NULL
 #' @return  predicted values
+#' 
+#' 
+#' 
+#' 
+#' 
+#' @examples
+#' Train the model
+#' generate some data
+#' set.seed(1235)
+#' N = 100 ; p =500;nz=4; K=nz
+#' X <- matrix(rnorm(n = N * p), nrow = N, ncol = p)
+#' mx=colMeans(X)
+#' sx=sqrt(apply(X,2,var))
+#' X=scale(X,mx,sx)
+#' X=matrix(as.numeric(X),N,p)
+#' Z =matrix(rnorm(N*nz),N,nz)
+#' mz=colMeans(Z)
+#' sz=sqrt(apply(Z,2,var))
+#' Z=scale(Z,mz,sz)
+#' beta_1 <- rep(x = 0, times = p); beta_2<-rep(x = 0, times = p);beta_3<-rep(x = 0, times = p);beta_4<-rep(x = 0, times = p);beta_5<-rep(x = 0, times = p);beta_6<-rep(x = 0, times = p)
+#' 
+#' 
+#' beta_1[1:5] <- c(2, 2, 2, 2,2); beta_2[1:5]<-c(2, 2, 2, 2,2); beta_3[6:10]<-c(2, 2, 2, -2,-2); beta_4[6:10] <- c(2, 2, 2, -2,-2); beta_5[11:15] <- c(-2,  -2,-2, -2,-2);beta_6[11:15] <- c(-2, -2, -2, -2,-2)
+#' 
+#' Beta<-cbind(beta_1,beta_2,beta_3,beta_4,beta_5,beta_6)
+#' colnames(Beta)<-c(1:6)
+#' 
+#' 
+#' theta<-array(0,c(p,K,6))
+#' theta[1,1,1]<-2;theta[3,2,1]<-2;theta[4,3,1]<- -2;theta[5,4,1]<- -2
+#' theta[1,1,2]<-2;theta[3,2,2]<-2;theta[4,3,2]<- -2;theta[5,4,2]<- -2
+#' theta[6,1,3]<-2;theta[8,2,3]<-2;theta[9,3,3]<- -2;theta[10,4,3]<- -2
+#' theta[6,1,4]<-2;theta[8,2,4]<-2;theta[9,3,4]<- -2;theta[10,4,4]<- -2
+#' theta[11,1,5]<-2;theta[13,2,5]<-2;theta[14,3,5]<- -2;theta[15,4,5]<- -2
+#' theta[11,1,6]<-2;theta[13,2,6]<-2;theta[14,3,6]<- -2;theta[15,4,6]<- -2
+#' 
+#' library(MASS)
+#' pliable = matrix(0,N,6)
+#' for (e in 1:6) {
+#' pliable[,e]<-	compute_pliable(X, Z, theta[,,e])
+#' }
+#' 
+#' esd<-diag(6)
+#' e<-mvrnorm(N,mu=rep(0,6),Sigma=esd)
+#' y_train<-X%*%Beta+pliable+e
+#' y=y_train
+#' #colnames(y)<-c(1:6)
+#' colnames(y)<- c( paste("y",1:(ncol(y)),sep = "") )
+#' TT=tree.parms(y)
+
+#' gg1<-c(.5,0.0001)
+#' cl=detectCores()
+#' nlambda = 50;e.abs=1E-3;e.rel=1E-3;alpha=.5
+#'  fit<-MADMMplasso(X,Z,y,alpha=alpha,my_lambda=NULL,lambda_min=0.01,max_it=5000,e.abs=e.abs,e.rel=e.rel,maxgrid=50,nlambda = nlambda, rho=5,tree = TT,my_print = F,alph=1,parallel =F,pal=1,gg=gg1,tol=1E-3,cl=cl-2 )
+#'  
+#'  
+#'  cv_admp<-cv.MADMMplasso(fit,nfolds=5,X,Z,y,alpha=alpha,lambda=fit$Lambdas,max_it=5000,e.abs=e.abs,e.rel=e.rel,nlambda, rho=5,my_print=F,alph=1,foldid=NULL,parallel = F,pal=1,gg=gg1,TT=TT,tol=1E-3)
+#'  
+#'  plot(cv_admp)
+
+
 
 #' @export
 
