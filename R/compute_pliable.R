e5fd068ce711a4a38c2581b36a874f9a6df2448d
Theophilus Quachie Asenso <85598983+Theo-qua@users.noreply.github.com>
2023-12-13 11:02:59 +0100

    Update compute_pliable.R
    Update the documentation of compute_pliable

--- R/compute_pliable.R
+++ R/compute_pliable.R
@@ -1,9 +1,10 @@
-#' @title TODO: fill this field
-#' @description TODO: fill this field
-#' @param X TODO: fill this field
-#' @param Z TODO: fill this field
-#' @param theta TODO: fill this field
-#' @return TODO: fill this field
+#' @title Compute the interaction part of the model.
+#' @description  Compute the interaction part of the model.
+#' @param X N by p matrix of predictors
+#' @param Z N by K matrix of modifying variables. The elements of Z  may represent quantitative or categorical variables, or a mixture of the two.
+#' Categorical variables should be coded by 0-1 dummy variables: for a k-level variable, one can use either k or k-1  dummy variables.
+#' @param theta    theta coefficients for a single response ncol(X) by ncol(Z)  
+#' @return a vector of length N of the calculated interaction term for a single response 
 #' @export
 compute_pliable <- function(X, Z, theta) {
   p <- ncol(X)
