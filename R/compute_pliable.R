3061f321db33c5497d88e9c2335e8b315ad40d1d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-01-12 13:50:50 +0100

    Fixed `trailing_whitespace_linter()` (#19)

--- R/compute_pliable.R
+++ R/compute_pliable.R
@@ -3,8 +3,8 @@
 #' @param X N by p matrix of predictors
 #' @param Z N by K matrix of modifying variables. The elements of Z  may represent quantitative or categorical variables, or a mixture of the two.
 #' Categorical variables should be coded by 0-1 dummy variables: for a k-level variable, one can use either k or k-1  dummy variables.
-#' @param theta    theta coefficients for a single response ncol(X) by ncol(Z)  
-#' @return a vector of length N of the calculated interaction term for a single response 
+#' @param theta    theta coefficients for a single response ncol(X) by ncol(Z)
+#' @return a vector of length N of the calculated interaction term for a single response
 #' @export
 compute_pliable <- function(X, Z, theta) {
   p <- ncol(X)
