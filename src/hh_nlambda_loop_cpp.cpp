411df0f383c99a08f4a94b66878fa4bfef5d5708
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-01-26 13:54:49 +0100

    Revert "Removed duplicated `if` case (#17)"
    This reverts commit 92eda0139517ad26680072f172dcdb7f3ad16a44.

--- src/hh_nlambda_loop_cpp.cpp
+++ src/hh_nlambda_loop_cpp.cpp
@@ -62,8 +62,8 @@ Rcpp::List hh_nlambda_loop_cpp(
       // theta0 <- my_values$theta0 ### iteration
       // beta_hat <- my_values$beta_hat
       // y_hat <- my_values$y_hat
-    } else if (parallel && pal == 0) {
-      // This is the default case.
+    }
+    if (parallel && pal == 0) {
       // beta <- my_values[hh, ]$beta
       // theta <- my_values[hh, ]$theta
       // my_obj[[hh]] <- list(my_values[hh, ]$obj)
@@ -71,8 +71,14 @@ Rcpp::List hh_nlambda_loop_cpp(
       // theta0 <- my_values[hh, ]$theta0 ### iteration
       // beta_hat <- my_values[hh, ]$beta_hat
       // y_hat <- my_values[hh, ]$y_hat
-    } else {
-      Rcpp::stop("Invalid combination of parallel and pal.");
+    } else if (parallel && pal == 0) {
+      // beta <- my_values[[hh]]$beta
+      // theta <- my_values[[hh]]$theta
+      // my_obj[[hh]] <- list(my_values[[hh]]$obj)
+      // beta0 <- my_values[[hh]]$beta0
+      // theta0 <- my_values[[hh]]$theta0 ### iteration
+      // beta_hat <- my_values[[hh]]$beta_hat
+      // y_hat <- my_values[[hh]]$y_hat
     }
 
     // beta1 <- as(beta * (abs(beta) > tol), "sparseMatrix")
