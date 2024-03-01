122566f25964d87cb6219a867dd8de8bb81272d7
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-03-01 14:18:24 +0100

    Added unit tests for #28

---
+++ tests/testthat/test-reg.R
@@ -0,0 +1,25 @@
+# Original function ============================================================
+reg_R <- function(r, Z) {
+  K <- ncol(Z)
+  N <- nrow(Z)
+  beta01 <- matrix(0, 1, ncol(r))
+  theta01 <- matrix(0, ncol(Z), ncol(r))
+  for (e in 1:ncol(r)) {
+    new1 <- lm(r[, e] ~ Z, singular.ok = TRUE)
+    beta01[e] <- matrix(new1$coefficients[1])
+    theta01[, e] <- as.vector(new1$coefficients[-1])
+  }
+  return(list(beta0 = beta01, theta0 = theta01))
+}
+
+# Testing ======================================================================
+reps <- 10L
+n_obs <- rpois(reps, lambda = 10L)
+n_vars <- sample(2:10, reps, replace = TRUE)
+test_that("reg() produces the correct output", {
+  for (rp in seq_len(reps)) {
+    r <- matrix(rnorm(n_obs[rp] * n_vars[rp]), n_obs[rp], n_vars[rp])
+    z <- as.matrix(sample(0:1, n_obs[rp], replace = TRUE))
+    expect_equal(reg(r, z), reg_R(r, z))
+  }
+})
