22be22e849843fc0a81925052071ae23a786cb8d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-08-20 16:13:56 +0200

    Fixed code smell

--- tests/testthat/test-issue_53.R
+++ tests/testthat/test-issue_53.R
@@ -67,7 +67,7 @@ esd <- diag(6)
 e <- MASS::mvrnorm(N, mu = rep(0, 6), Sigma = esd)
 y_train <- X %*% Beta + pliable + e
 y <- y_train
-colnames(y) <- paste0("y", 1:(ncol(y)))
+colnames(y) <- paste0("y", seq_len(ncol(y)))
 TT <- tree_parms(y)
 
 gg1 <- matrix(0, 2, 2)
